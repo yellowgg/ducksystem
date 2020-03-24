3e190f2b09e960cd9537d8039765361516f45cc7
yellowgg <yellowgggg@gmail.com>
2020-03-24 19:01:00 +0800

    丝袜哥配置类

---
+++ src/main/java/cn/yellowgg/ducksystem/config/Swagger2Config.java
@@ -0,0 +1,37 @@
+package cn.yellowgg.ducksystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * @Description:丝袜哥配置类
+ * @Author: yellowgg
+ * @Date: Created in 2020/3/24 15:27
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("cn.yellowgg.ducksystem.controller"))
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("小黄鸭天下第一").description("描述：🐥🐥小黄鸭IT培训机构后台管理系统的文档🐥🐥")
+                .termsOfServiceUrl("https://github.com/yellowgg").version("1.0.0")
+                .contact(new Contact("yellowgg", "", "yellowgggg@gmail.com")).build();
+    }
+}

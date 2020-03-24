a66794d6e7dad8bbec42286487c45ee3b58ddfb5
yellowgg <yellowgggg@gmail.com>
2020-03-24 13:50:28 +0800

    项目初始化

---
+++ src/main/java/cn/yellowgg/ducksystem/DucksystemApplication.java
@@ -0,0 +1,13 @@
+package cn.yellowgg.ducksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DucksystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DucksystemApplication.class, args);
+    }
+
+}

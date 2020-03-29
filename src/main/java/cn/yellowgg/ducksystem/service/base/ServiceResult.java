b3d0c3217c6e19068571760ce8da7fe8e311186d
yellowgg <yellowgggg@gmail.com>
2020-03-30 02:38:14 +0800

    自定义响应结构

--- src/main/java/cn/yellowgg/ducksystem/service/base/ServiceResult.java
+++ src/main/java/cn/yellowgg/ducksystem/service/base/ServiceResult.java
@@ -1,18 +1,13 @@
 package cn.yellowgg.ducksystem.service.base;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import cn.hutool.json.JSONUtil;
+import cn.yellowgg.ducksystem.constant.UtilConstants;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
  * @Description: 自定义响应结构
- * 前端接受此类数据（json object)后，可自行根据业务去实现相关功能
- * 200：表示成功
- * 500：表示错误，错误信息在msg字段中
- * 501：bean验证错误，不管多少个错误都以map形式返回
- * 502：拦截器拦截到用户token出错
- * 555：异常抛出信息
  * @Author: yellowgg
  * @Date: Created in 2020/3/27 15:08
  */
@@ -20,77 +15,38 @@ import lombok.Data;
 @ApiModel(value = "自定义响应结构类")
 public class ServiceResult {
 
-    // 定义jackson对象
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    @ApiModelProperty(value = "响应码")
+    private Integer respCode;
 
-    // 响应业务状态
-    private Integer status;
-
-    // 响应消息
+    @ApiModelProperty(value = "附属消息")
     private String msg;
 
-    // 响应中的数据
-    private Object data;
+    @ApiModelProperty(value = "数据")
+    private Object obj;
 
-    @JsonIgnore
-    private String ok; // 不使用
+    @ApiModelProperty(value = "请求结果")
+    private boolean success;
 
-    public static ServiceResult build(Integer status, String msg, Object data) {
-        return new ServiceResult(status, msg, data);
-    }
-
-    public static ServiceResult build(Integer status, String msg, Object data, String ok) {
-        return new ServiceResult(status, msg, data, ok);
-    }
-
-    public static ServiceResult ok(Object data) {
-        return new ServiceResult(data);
-    }
-
-    public static ServiceResult ok() {
-        return new ServiceResult(null);
-    }
-
-    public static ServiceResult errorMsg(String msg) {
-        return new ServiceResult(500, msg, null);
-    }
-
-    public static ServiceResult errorMap(Object data) {
-        return new ServiceResult(501, "error", data);
-    }
-
-    public static ServiceResult errorTokenMsg(String msg) {
-        return new ServiceResult(502, msg, null);
-    }
-
-    public static ServiceResult errorException(String msg) {
-        return new ServiceResult(555, msg, null);
-    }
-
-    public ServiceResult() {
-    }
-
-    public ServiceResult(Integer status, String msg, Object data) {
-        this.status = status;
+    private ServiceResult(Integer respCode, String msg, Object obj, boolean success) {
+        this.respCode = respCode;
         this.msg = msg;
-        this.data = data;
+        this.obj = obj;
+        this.success = success;
     }
 
-    public ServiceResult(Integer status, String msg, Object data, String ok) {
-        this.status = status;
-        this.msg = msg;
-        this.data = data;
-        this.ok = ok;
+    public static ServiceResult asSuccess(Object obj) {
+        return new ServiceResult(UtilConstants.RespCode.SUCCESS, null, obj, true);
     }
 
-    public ServiceResult(Object data) {
-        this.status = 200;
-        this.msg = "OK";
-        this.data = data;
+    public static ServiceResult asSuccess(Object obj, String msg) {
+        return new ServiceResult(UtilConstants.RespCode.SUCCESS, msg, obj, true);
     }
 
-    public Boolean isOK() {
-        return this.status == 200;
+    public static ServiceResult asFail(String msg) {
+        return new ServiceResult(UtilConstants.RespCode.SUCCESS, msg, null, false);
     }
 
+    public String toJson() {
+        return JSONUtil.toJsonStr(this);
+    }
 }

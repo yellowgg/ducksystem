7d98922b6dc1324babab440b7977c4d47a276022
yellowgg <yellowgggg@gmail.com>
2020-03-31 16:45:16 +0800

    启动类去掉热部署 热部署会导致很多问题，测试的时候去掉

--- src/main/java/cn/yellowgg/ducksystem/DucksystemApplication.java
+++ src/main/java/cn/yellowgg/ducksystem/DucksystemApplication.java
@@ -11,6 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 public class DucksystemApplication {
 
     public static void main(String[] args) {
+        // TODO yellowgg 热部署会导致很多东西有毛病，测试的时候加上注释
+        /**
+         * 已知错误：1. shiro的标签会失效
+         */
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(DucksystemApplication.class, args);
     }
 

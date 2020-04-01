7a1aa67b8c82723c207f80f3dc122859cd48ed5a
yellowgg <yellowgggg@gmail.com>
2020-04-02 01:09:29 +0800

    全局异常

--- src/main/java/cn/yellowgg/ducksystem/exception/CustomExceptionResolver.java
+++ src/main/java/cn/yellowgg/ducksystem/exception/CustomExceptionResolver.java
@@ -4,6 +4,7 @@ import cn.hutool.http.HttpStatus;
 import cn.yellowgg.ducksystem.service.base.ServiceResult;
 import cn.yellowgg.ducksystem.utils.LogUtils;
 import org.slf4j.Logger;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -33,14 +34,23 @@ public class CustomExceptionResolver {
 
     /**
      * 拦截捕捉 参数校验异常
-     *
-     * @param ex javax.validation.ConstraintViolationException
      */
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler({ConstraintViolationException.class})
     public @ResponseBody
     String constraintViolationExceptionHandler(ConstraintViolationException ex) {
         return ServiceResult.asFail(HttpStatus.HTTP_PRECON_FAILED, ex.getConstraintViolations().stream()
                 .map(x -> x.getMessage()).collect(Collectors.joining(","))).toJson();
     }
+
+    /**
+     * 拦截捕捉 参数校验异常
+     */
+    @ExceptionHandler({BindException.class})
+    public @ResponseBody
+    String bindExceptionHandler(BindException ex) {
+        return ServiceResult.asFail(HttpStatus.HTTP_PRECON_FAILED, ex.getBindingResult().getAllErrors().stream()
+                .map(x -> x.getDefaultMessage()).collect(Collectors.joining(","))).toJson();
+    }
+
 
 }

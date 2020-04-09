84c2b876d8663e1b27be28e501fed6fde415a40f
yellowgg <yellowgggg@gmail.com>
2020-04-09 22:21:18 +0800

    Base64注解

--- src/main/java/cn/yellowgg/ducksystem/annotation/Base64DecodeStr.java
+++ src/main/java/cn/yellowgg/ducksystem/annotation/Base64DecodeStr.java
@@ -7,14 +7,21 @@ import java.lang.annotation.Target;
 
 /**
  * @Description: 自定义注解 用于Base64解密
+ * 暂不能跟@PathVariable一起用
  * @Author: yellowgg
  * @Date: Created in 2020/4/3 15:07
  */
-@Target(ElementType.PARAMETER)
+@Target({ElementType.METHOD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Base64DecodeStr {
+
     /**
      * 解析次数
      */
-    int count() default 1;
+    int count() default 3;
+
+    /**
+     * 如果作用于对象参数的话，排除对象哪些字段不用解密
+     */
+    String[] excludeField() default {};
 }
--- src/main/java/cn/yellowgg/ducksystem/interceptor/Base64DecodeStrResolver.java
+++ src/main/java/cn/yellowgg/ducksystem/interceptor/Base64DecodeStrResolver.java
@@ -1,7 +1,12 @@
 package cn.yellowgg.ducksystem.interceptor;
 
-import cn.hutool.core.codec.Base64;
+import cn.hutool.core.util.ArrayUtil;
 import cn.yellowgg.ducksystem.annotation.Base64DecodeStr;
+import cn.yellowgg.ducksystem.entity.base.BaseEntity;
+import cn.yellowgg.ducksystem.utils.Base64Utils;
+import cn.yellowgg.ducksystem.utils.LogUtils;
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -14,18 +19,43 @@ import org.springframework.web.method.support.ModelAndViewContainer;
  * @Date: Created in 2020/4/3 15:28
  */
 public class Base64DecodeStrResolver implements HandlerMethodArgumentResolver {
+
+    private static final transient Logger log = LogUtils.getExceptionLogger();
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(Base64DecodeStr.class);
+        return parameter.hasMethodAnnotation(Base64DecodeStr.class) || parameter.hasParameterAnnotation(Base64DecodeStr.class);
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
                                   WebDataBinderFactory binderFactory) throws Exception {
-        String value = webRequest.getParameter(parameter.getParameterName());
-        for (int i = 0; i < parameter.getParameterAnnotation(Base64DecodeStr.class).count(); i++) {
-            value = Base64.decodeStr(value);
+        int count = parameter.hasParameterAnnotation(Base64DecodeStr.class)
+                ? parameter.getParameterAnnotation(Base64DecodeStr.class).count()
+                : parameter.getMethodAnnotation(Base64DecodeStr.class).count();
+        String[] excludeFields = parameter.hasParameterAnnotation(Base64DecodeStr.class)
+                ? parameter.getParameterAnnotation(Base64DecodeStr.class).excludeField()
+                : parameter.getMethodAnnotation(Base64DecodeStr.class).excludeField();
+        // 参数类型
+        if (BaseEntity.class.isAssignableFrom(parameter.getParameterType())) {
+            Object obj = parameter.getParameterType().newInstance();
+            webRequest.getParameterMap().forEach((k, v) -> {
+                try {
+                    if (ArrayUtil.contains(excludeFields, k)) {
+                        BeanUtils.setProperty(obj, k, v[0]);
+                    } else {
+                        BeanUtils.setProperty(obj, k, Base64Utils.decodeStrofCount(v[0], count));
+                    }
+                } catch (Exception e) {
+                    log.error("参数解码有误", e);
+                    throw new RuntimeException("BeanUtils.setProperty错误");
+                }
+            });
+            return obj;
+        } else if (!Iterable.class.isAssignableFrom(parameter.getParameterType())) {
+            return Base64Utils.decodeStrofCount((webRequest.getParameter(parameter.getParameterName())), count);
         }
-        return value;
+        throw new RuntimeException("参数解码有误");
     }
+
 }

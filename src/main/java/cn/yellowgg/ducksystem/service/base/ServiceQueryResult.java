7527ba715ccb8bfbf5fb7046e4f50887843ee32e
yellowgg <yellowgggg@gmail.com>
2020-04-13 00:26:33 +0800

    自定义响应结构——List

---
+++ src/main/java/cn/yellowgg/ducksystem/service/base/ServiceQueryResult.java
@@ -0,0 +1,45 @@
+package cn.yellowgg.ducksystem.service.base;
+
+import cn.hutool.http.HttpStatus;
+import com.github.pagehelper.PageInfo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Description: 自定义响应结构，带list
+ * @Author: yellowgg
+ * @Date: Created in 2020/4/11 22:11
+ */
+@Data
+@ApiModel(value = "自定义响应结构,带List")
+public class ServiceQueryResult<T> {
+
+    @ApiModelProperty(value = "响应码")
+    private Integer respCode;
+
+    @ApiModelProperty(value = "附属消息")
+    private String msg;
+
+    @ApiModelProperty(value = "数据")
+    private List<T> obj;
+
+    @ApiModelProperty(value = "请求结果")
+    private boolean success;
+
+    private long totalCount;
+
+    public ServiceQueryResult(Integer respCode, String msg, List<T> obj, boolean success, long totalCount) {
+        this.respCode = respCode;
+        this.msg = msg;
+        this.obj = obj;
+        this.success = success;
+        this.totalCount = totalCount;
+    }
+
+    public static <T> ServiceQueryResult asSuccess(PageInfo<T> pageInfo, String msg) {
+        return new ServiceQueryResult(HttpStatus.HTTP_OK, msg, pageInfo.getList(), true, pageInfo.getTotal());
+    }
+}

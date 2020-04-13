e444453867cb1227127bd5a86f8038d8d2f78990
yellowgg <yellowgggg@gmail.com>
2020-04-14 01:34:29 +0800

    接口遵循正确错误原则

--- src/main/java/cn/yellowgg/ducksystem/controller/portal/TeacherController.java
+++ src/main/java/cn/yellowgg/ducksystem/controller/portal/TeacherController.java
@@ -3,12 +3,18 @@ package cn.yellowgg.ducksystem.controller.portal;
 import cn.yellowgg.ducksystem.entity.Teacher;
 import cn.yellowgg.ducksystem.service.TeacherService;
 import cn.yellowgg.ducksystem.service.base.ServiceQueryResult;
+import cn.yellowgg.ducksystem.service.base.ServiceResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * @Description:
@@ -26,6 +32,14 @@ public class TeacherController {
     @ApiOperation("获取教师信息列表")
     @GetMapping("/allData")
     public ServiceQueryResult<Teacher> getData() {
-        return ServiceQueryResult.asSuccess(teacherService.queryAll());
+        List<Teacher> result = teacherService.queryAll();
+        return CollectionUtils.isNotEmpty(result) ? ServiceQueryResult.asSuccess(result) : ServiceQueryResult.asFail("暂无数据");
+    }
+
+    @ApiOperation("获取教师信息")
+    @GetMapping("/getInfo/{teacherId}")
+    public ServiceResult<Teacher> getTeacherInfo(@PathVariable String teacherId) {
+        Teacher teacher = teacherService.selectByPrimaryKey(Long.parseLong(teacherId));
+        return Objects.nonNull(teacher) ? ServiceResult.asSuccess(teacher) : ServiceResult.asFail("老师不存在");
     }
 }

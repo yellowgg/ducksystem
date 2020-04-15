656e556ec3d07cbec786659f5129927e7a72b125
yellowgg <yellowgggg@gmail.com>
2020-04-15 22:21:55 +0800

    课程CURD

---
+++ src/main/java/cn/yellowgg/ducksystem/controller/CourseMgr.java
@@ -0,0 +1,60 @@
+package cn.yellowgg.ducksystem.controller;
+
+import cn.yellowgg.ducksystem.constant.UtilConstants;
+import cn.yellowgg.ducksystem.entity.Course;
+import cn.yellowgg.ducksystem.enums.CourseTypeEnum;
+import cn.yellowgg.ducksystem.service.CourseService;
+import cn.yellowgg.ducksystem.service.base.ServiceQueryResult;
+import cn.yellowgg.ducksystem.service.base.ServiceResult;
+import cn.yellowgg.ducksystem.utils.Base64Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+/**
+ * @Description: 课程后台
+ * @Author: yellowgg
+ * @Date: Created in 2020/4/15 15:42
+ */
+@Controller
+@RequestMapping("/course")
+public class CourseMgr {
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("/page")
+    public String page(Model model) {
+        Map<Integer, String> map = CourseTypeEnum.getValueAndNameMap();
+        model.addAttribute("courseTypes", map);
+        return "courseList";
+    }
+
+    @GetMapping("/data")
+    @ResponseBody
+    public ServiceQueryResult getData(@RequestParam(defaultValue = "1") Integer pageNum,
+                                      @RequestParam(defaultValue = "10") Integer pageSize,
+                                      Course course) {
+        return ServiceQueryResult.asSuccess(courseService.queryByAllSelectiveOrderByIdwithPage(pageNum, pageSize, course));
+    }
+
+    @PostMapping("/addOrUp")
+    @ResponseBody
+    public ServiceResult insertOrUpdateSelective(@Valid Course course) {
+        return courseService.insertOrUpdateSelective(course) > UtilConstants.Number.ZERO
+                ? ServiceResult.asSuccess(null, "操作成功")
+                : ServiceResult.asFail("操作失败");
+    }
+
+    @PostMapping("/del/{id}")
+    @ResponseBody
+    public ServiceResult del(@PathVariable String id) {
+        return courseService.deleteByPrimaryKey(Long.parseLong(Base64Utils.decodeStrofCount(id,
+                UtilConstants.Number.THREE))) > UtilConstants.Number.ZERO
+                ? ServiceResult.asSuccess(null, "删除成功")
+                : ServiceResult.asFail("删除失败,请稍后重试");
+    }
+}
--- src/main/java/cn/yellowgg/ducksystem/enums/CourseTypeEnum.java
+++ src/main/java/cn/yellowgg/ducksystem/enums/CourseTypeEnum.java
@@ -1,6 +1,9 @@
 package cn.yellowgg.ducksystem.enums;
 
 import cn.yellowgg.ducksystem.constant.UtilConstants;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
 
 /**
  * @Description: 课程类型枚举
@@ -22,5 +25,22 @@ public enum CourseTypeEnum {
     CourseTypeEnum(Integer value, String name) {
         this.value = value;
         this.name = name;
+    }
+
+    public static String getNameByValue(Integer value) {
+        for (CourseTypeEnum each : CourseTypeEnum.values()) {
+            if (each.value.equals(value)) {
+                return each.name;
+            }
+        }
+        return "课程类型异常";
+    }
+
+    public static Map<Integer, String> getValueAndNameMap() {
+        Map<Integer, String> map = Maps.newHashMap();
+        for (CourseTypeEnum each : CourseTypeEnum.values()) {
+            map.put(each.value, each.name);
+        }
+        return map;
     }
 }

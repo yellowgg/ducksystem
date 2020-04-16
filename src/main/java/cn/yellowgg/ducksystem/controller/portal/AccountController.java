706940704df17dfdd5695a1167fa2cbbb34c1f36
yellowgg <yellowgggg@gmail.com>
2020-04-17 00:10:46 +0800

    适配小程序初始化 初始化分成身份进入，所以后台逻辑修改一些

--- src/main/java/cn/yellowgg/ducksystem/controller/portal/AccountController.java
+++ src/main/java/cn/yellowgg/ducksystem/controller/portal/AccountController.java
@@ -6,12 +6,17 @@ import cn.yellowgg.ducksystem.service.AccountService;
 import cn.yellowgg.ducksystem.service.base.ServiceResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * @Author: yellowgg
@@ -20,6 +25,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/accountPortal")
 @Api(tags = "用户信息门户")
+@Validated
 public class AccountController {
     @Autowired
     AccountService accountService;
@@ -30,5 +36,16 @@ public class AccountController {
         return accountService.register(accout) > UtilConstants.Number.ZERO
                 ? ServiceResult.asSuccess(null, "保存成功")
                 : ServiceResult.asFail("保存失败");
+    }
+
+    @ApiOperation(value = "保存微信用户的身份", notes = "如果没有用户信息设置success为false且只返回openid")
+    @PostMapping("/saveRole")
+    @ResponseBody
+    public ServiceResult insertOpenIdAndisAdmin(@NotBlank(message = "openid不能为空") String openId,
+                                                @NotNull(message = "isAdmin不能为空") @Range(max = 1) Integer isAdmin) {
+        return accountService.addRole(Account.init(openId, isAdmin)) > UtilConstants.Number.ZERO
+                ? ServiceResult.asSuccess(null, "保存成功")
+                : ServiceResult.asFail("保存失败");
+
     }
 }
